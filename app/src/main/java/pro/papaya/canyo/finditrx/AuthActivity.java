69e6699c22a9ff776c75d9117e1bd78f51e2ca64
Vladislav Erchik <vladislav.erchik@exposit.com>
2019-03-26 17:07:44 +0300

    Setting up firebase. Fix layout

--- app/src/main/java/pro/papaya/canyo/finditrx/AuthActivity.java
+++ app/src/main/java/pro/papaya/canyo/finditrx/AuthActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AuthActivity extends AppCompatActivity {
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
