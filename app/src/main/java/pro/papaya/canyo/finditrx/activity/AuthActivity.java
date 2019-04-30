636a0ec53f6a5b4614e9c91c5d74e0c5a8183c38
Vladislav Erchik <canyousaaay@gmail.com>
2019-04-30 15:51:05 +0300

    Add logo. Refactor login page

--- app/src/main/java/pro/papaya/canyo/finditrx/activity/AuthActivity.java
+++ app/src/main/java/pro/papaya/canyo/finditrx/activity/AuthActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.lifecycle.ViewModelProviders;
+
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-import androidx.lifecycle.ViewModelProviders;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.SingleObserver;
